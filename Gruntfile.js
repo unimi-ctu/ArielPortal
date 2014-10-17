722dac79b9d34974adc1897ec41b67d79655ceba
Alessandro Muiesan <ale@ObliotBook.local>
2014-10-17 16:50:08 +0200

    ale

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2014-10-14 using generator-angular 0.9.8
+// Generated on 2014-10-17 using generator-angular 0.9.8
 'use strict';
 
 // # Globbing
@@ -68,7 +68,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
