782b60042762f1e0d6cbb0e07c76e1e8c6d0aea0
Alessandro Muiesan <ale@ctu.unimi.it>
2014-11-13 16:46:35 +0100

    aggiunge il filtro w4

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -44,6 +44,35 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+
+      var $http,
+          interceptor = ['$q', '$injector', function ($q, $injector) {
+            function success(response) {
+                // get $http via $injector because of circular dependency problem
+                $http = $http || $injector.get('$http');
+                if($http.pendingRequests.length < 1) {
+                    angular.element('#loadingWidget').hide();
+                }
+                return response;
+            }
+
+            function error(response) {
+                // get $http via $injector because of circular dependency problem
+                $http = $http || $injector.get('$http');
+                if($http.pendingRequests.length < 1) {
+                    angular.element('#loadingWidget').hide();
+                }
+                console.log('error detected');
+                return $q.reject(response);
+            }
+
+            return function (promise) {
+                angular.element('#loadingWidget').show();
+                return promise.then(success, error);
+            };
+          }];
+
+      $httpProvider.responseInterceptors.push(interceptor);
   })
   .run(function ($rootScope, $route, $location, portal) {
     $rootScope.isDebug = false;
@@ -75,3 +104,4 @@ angular
 
 
   });
+
